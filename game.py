e9a830d109985e453c67c79d2ac8ed0a7e480a5a
Vladimir <vladimirrybakov123@gmail.com>
2024-06-25 22:45:49 +0100

    generate code for the color of the game

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
 import random
+
+COLORS = ["R", "G", "B", "Y", "W", "O"]
+TRIES = 10
+CODE_LENGTH = 4
+
+def generate_code():
+    code = []
+
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+    
+    return code
