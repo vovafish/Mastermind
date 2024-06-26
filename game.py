d356165fc13dec8993adfc466d37d4edbb36ae9b
Vladimir <vladimirrybakov123@gmail.com>
2024-06-26 16:21:15 +0100

    created guess code func that guesses the provided colors

--- game.py
+++ game.py
@@ -12,3 +12,13 @@ def generate_code():
         code.append(color)
     
     return code
+
+def guess_code():
+
+    while True:
+        guess = input("Guess: ").upper().split(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess ${CODE_LENGTH} colors.")
+            continue
+
